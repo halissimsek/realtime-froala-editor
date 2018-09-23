455aea04129b970d1fed20061faba184e7ee2a17
halissimsek <halis.simsek@gmail.com>
2018-09-23 10:07:26 +0300

    Selection added.

--- public/sketch.js
+++ public/sketch.js
@@ -12,11 +12,20 @@ function setup(){
         };
         socket.emit('text', data);
     });
+    
+    $('#text').on('froalaEditor.mousedown', function (e, editor, mouseupEvent) {
+        var data = {
+            selection : $('#text').froalaEditor('selection.blocks')
+        };
+                socket.emit('cursor', data);
+     });
+    
     $('#text').froalaEditor({
         toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color', 'inlineStyle', 'paragraphStyle', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-', 'insertLink', 'insertImage', 'insertVideo', 'insertFile', 'insertTable', '|', 'emoticons', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|', 'print', 'help', 'html', '|', 'undo', 'redo'],
         fullPage: true
     });
 
+    socket.on('cursor', setSelectionText);
     socket.on('text', handleRecievedText);
     socket.on('newUser', updateText);
 }
@@ -37,7 +46,12 @@ function handleRecievedText(data){
     //editor.selection.setAtEnd(editor.$el.get(0));
    // editor.selection.restore();
 
-    $('#text').froalaEditor('selection.setAfter', $('#text').froalaEditor('selection.blocks'));
+
+}
+
+function setSelectionText(data)
+{
+    $('#text').froalaEditor('selection.setAfter', data.selection);
     $('#text').froalaEditor('selection.restore');
 }
 
